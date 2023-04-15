8356dd44d8a338a1de224e8e6e7b0d3a77c26d05
Syncr0nate <danangpostman37@gmail.com>
2023-04-16 00:07:42 +0700

    Fix: fix an conflict

--- app/posts/[slug]/page.jsx
+++ app/posts/[slug]/page.jsx
@@ -12,11 +12,7 @@ export default function Post({ params }) {
   const post = getPost(slug);
 
   return (
-<<<<<<< HEAD
-    <article className="prose-lg lg:prose-xl min-h-screen mx-auto p-10 mt-10 max-w-none md:w-[700px] lg:w-[900px] overflow-hidden prose-headings:text-indigo-600 prose-pre:bg-indigo-900 prose-pre:text-indigo-50">
-=======
     <article className="prose prose-zinc lg:prose-xl min-h-screen mx-auto p-10 mt-10 max-w-none md:w-[769px] lg:w-[900px] overflow-hidden">
->>>>>>> 7b4442d88bd4fd431075c226055474117f3c242e
       <header className="border-b-2 pb-10">
         <h1 className="underline underline-offset-[5px]">{post.title}</h1>
         <h3 className="font-thin">{post.subTitle}</h3>
